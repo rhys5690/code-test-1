736e77cdfa87b32e79e38d8d6f818a9cb01084b2
rhys5690 <rhys5690@gmail.com>
2017-03-04 10:23:43 +1100

    I DID ITTTTTTT! :)

--- js/main.js
+++ js/main.js
@@ -1,20 +1,57 @@
-console.log($);
-console.log("yoyoyooyo");
 
-const givenName = document.querySelector('.given-name');
-const emailAddress = document.querySelector('.email-address');
+//create variable for each input field in the form
+
+var givenName = document.querySelector('.given-name');
+var surname = document.querySelector('.surname');
+var emailAddress = document.querySelector('.email-address');
+
+var allInputForms = document.querySelectorAll('input');
 
 
 
 
-function displayLetter() {
-  let currentEmail = emailAddress.value;
+// function displayEmail() {
+//   var currentEmail = emailAddress.value;
+//   var emailAddressDisplay = document.querySelector('.email-address-display');
+//   emailAddressDisplay.innerHTML =  currentEmail;
+// }
+//
+// //Event listeners for any information put in form
+//
+// // givenName.addEventListener('keyup', displayName);
+// emailAddress.addEventListener('keyup', displayEmail);
 
-  let emailAddressDisplay = document.querySelector('.email-address-display');
 
-  emailAddressDisplay.innerHTML =  `${currentEmail}`;
 
-};
 
-givenName.addEventListener('keyup', displayLetter);
-emailAddress.addEventListener('keyup', displayLetter);
+///Attempt all in one javascript
+
+
+//Create if statements to verify which card field to append text
+
+
+
+
+//return the field to display the information on the business card
+
+function findField(selectedInput) {
+  console.log("hello");
+
+  if (selectedInput.classList.contains("email-address") === true) {
+      return document.querySelector('.email-address-display');
+  } else {
+      return givenName;
+  }
+}
+
+
+function displayInput() {
+  var selectedInput = this;
+  var displayOnCard = this.value;
+  var fieldToDisplay = findField(selectedInput);
+  fieldToDisplay.innerHTML =  displayOnCard;
+}
+
+allInputForms.forEach(input => {
+  input.addEventListener('keyup', displayInput);
+});
