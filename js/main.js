31f1afc37181c5c1cf8a41e03c8da92e3a529ee6
rhys5690 <rhys5690@gmail.com>
2017-03-03 17:14:14 +1100

    begun adding javascript

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
 console.log($);
+console.log("yoyoyooyo");
+
+const givenName = document.querySelector('.given-name');
+const emailAddress = document.querySelector('.email-address');
+
+
+
+
+function displayLetter() {
+  let currentEmail = emailAddress.value;
+
+  let emailAddressDisplay = document.querySelector('.email-address-display');
+
+  emailAddressDisplay.innerHTML =  `${currentEmail}`;
+
+};
+
+givenName.addEventListener('keyup', displayLetter);
+emailAddress.addEventListener('keyup', displayLetter);
