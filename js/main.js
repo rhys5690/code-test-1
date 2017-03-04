05b2649cdd9f1f08f8254608cb9769e490d362d8
rhys5690 <rhys5690@gmail.com>
2017-03-04 11:20:54 +1100

    got entire form working

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-
 //create variable for each input field in the form
 
 var givenName = document.querySelector('.given-name');
@@ -6,9 +5,6 @@ var surname = document.querySelector('.surname');
 var emailAddress = document.querySelector('.email-address');
 
 var allInputForms = document.querySelectorAll('input');
-
-
-
 
 // function displayEmail() {
 //   var currentEmail = emailAddress.value;
@@ -21,37 +17,42 @@ var allInputForms = document.querySelectorAll('input');
 // // givenName.addEventListener('keyup', displayName);
 // emailAddress.addEventListener('keyup', displayEmail);
 
-
-
-
 ///Attempt all in one javascript
 
-
 //Create if statements to verify which card field to append text
-
-
-
 
 //return the field to display the information on the business card
 
 function findField(selectedInput) {
-  console.log("hello");
 
-  if (selectedInput.classList.contains("email-address") === true) {
-      return document.querySelector('.email-address-display');
-  } else {
-      return givenName;
-  }
+    if (selectedInput.classList.contains("email-address")) {
+        return document.querySelector('.email-address-display');
+    } else if (selectedInput.classList.contains("phone")) {
+        return document.querySelector(".phone-display");
+    } else if (selectedInput.classList.contains("house-number")) {
+        return document.querySelector(".house-number-display");
+    } else if (selectedInput.classList.contains("street")) {
+        return document.querySelector(".street-display");
+    } else if (selectedInput.classList.contains("suburb")) {
+        return document.querySelector(".suburb-display");
+    } else if (selectedInput.classList.contains("state")) {
+        return document.querySelector(".state-display");
+    } else if (selectedInput.classList.contains("post-code")) {
+        return document.querySelector(".post-code-display");
+    } else if (selectedInput.classList.contains("country")) {
+        return document.querySelector(".country-display");
+    } else {
+        return console.log("ERROR, not a field");
+    }
 }
 
-
 function displayInput() {
-  var selectedInput = this;
-  var displayOnCard = this.value;
-  var fieldToDisplay = findField(selectedInput);
-  fieldToDisplay.innerHTML =  displayOnCard;
+    var selectedInput = this;
+    var displayOnCard = this.value;
+    var fieldToDisplay = findField(selectedInput);
+    fieldToDisplay.innerHTML = displayOnCard;
 }
 
 allInputForms.forEach(input => {
-  input.addEventListener('keyup', displayInput);
+    input.addEventListener('keyup', displayInput);
 });
